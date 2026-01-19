736152debc5361399b00d049536c0a6f68e2ed31
Osman <Namsomata@gmail.com>
2026-01-19 23:36:54 +0000

    [story-4] Add repository interface to decouple data layer

--- app/src/main/java/co/uk/next/techtest/repository/ProductsRepository.kt
+++ app/src/main/java/co/uk/next/techtest/repository/ProductsRepository.kt
@@ -3,9 +3,21 @@ package co.uk.next.techtest.repository
 import co.uk.next.techtest.data.Product
 import co.uk.next.techtest.remote.FakeStoreApiClient
 
-class ProductsRepository(private val apiClient: FakeStoreApiClient) {
 
-    suspend fun getProducts(): List<Product> {
+
+interface ProductsRepository {
+    suspend fun getProducts(): List<Product>
+    suspend fun getProduct(id: Int): Product
+}
+class ProductsRepositoryImpl(
+    private val apiClient: FakeStoreApiClient
+): ProductsRepository {
+
+    override suspend fun getProducts(): List<Product> {
         return apiClient.getAllProducts().products
+    }
+
+    override suspend fun getProduct(id: Int): Product {
+        return apiClient.getProduct(id)
     }
 }
