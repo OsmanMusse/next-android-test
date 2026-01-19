aaf2541c298180fa7a1d4f004535ba5ea8e0de50
Osman <Namsomata@gmail.com>
2026-01-19 16:01:45 +0000

    Introduce Scaffold layout to handle system bar insets correctly

--- app/src/main/java/co/uk/next/techtest/ui/products/ProductsScreen.kt
+++ app/src/main/java/co/uk/next/techtest/ui/products/ProductsScreen.kt
@@ -1,21 +1,33 @@
 package co.uk.next.techtest.ui.products
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
 fun ProductsScreen(viewModel: ProductsViewModel = viewModel()) {
     val products by viewModel.products.collectAsState()
 
-    LazyColumn {
-        items(products) { product ->
-            Text(text = product.title, color = MaterialTheme.colorScheme.onSurface)
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+    ) { paddingValues ->
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+        ) {
+            items(products) { product ->
+                Text(text = product.title, color = MaterialTheme.colorScheme.onSurface)
+            }
         }
     }
 }
