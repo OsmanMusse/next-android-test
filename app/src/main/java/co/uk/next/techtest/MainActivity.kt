f981a2c50eef9a1035d2b00f495cc5df6ba69ea1
Osman <Namsomata@gmail.com>
2026-01-19 23:34:51 +0000

    [story-4] move files into their corresponding package

--- app/src/main/java/co/uk/next/techtest/MainActivity.kt
+++ app/src/main/java/co/uk/next/techtest/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import co.uk.next.techtest.navigation.NextNavHost
 import co.uk.next.techtest.ui.theme.NextTakeHomeTestTheme
 
 
--- app/src/main/java/co/uk/next/techtest/di/NextApplication.kt
+++ app/src/main/java/co/uk/next/techtest/di/NextApplication.kt
@@ -1,7 +1,6 @@
-package co.uk.next.techtest
+package co.uk.next.techtest.di
 
 import android.app.Application
-import co.uk.next.techtest.di.appModules
 import org.koin.core.context.startKoin
 
 class NextApplication: Application() {
